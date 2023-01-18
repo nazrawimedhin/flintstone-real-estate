4bfdd29636f985497dd00f51c27a7a5d8208c62a
nazrawimedhin <nazrawimedhin1@gmail.com>
2023-01-18 18:27:38 +0300

    minor fix

--- src/components/home/team/Team.jsx
+++ src/components/home/team/Team.jsx
@@ -9,7 +9,6 @@ const Team = () => {
       <section className='team background'>
         <div className='container'>
           <Heading title='Our Featured Agents' subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.' />
-
           <div className='content mtop grid3'>
             {team.map((val, index) => (
               <div className='box' key={index}>
@@ -22,14 +21,13 @@ const Team = () => {
                   <i className='fa fa-location-dot'></i>
                   <label>{val.address}</label>
                   <h4>{val.name}</h4>
-
                   <ul>
                     {val.icon.map((icon, index) => (
                       <li key={index}>{icon}</li>
                     ))}
                   </ul>
                   <div className='button flex'>
-                    <button className='btn blue'>
+                    <button className='btn'>
                       <i className='fa fa-envelope'></i> Message
                     </button>
                     <button className='btn4'>
